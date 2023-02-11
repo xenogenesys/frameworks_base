f7d68cb3d50e7dc019357c28ff28d8e959f5996f
minaripenguin <minaripenguin@users.noreply.github.com>
2023-02-11 10:51:51 +0800

    core: Implement system vibration haptics settings
    * insipired from oplus haptics, based from aosp vibration effecs handling
    
    Co-authored-by: Blaster4385 <venkateshchaturvedi12@gmail.com>
    Change-Id: I9d928a902b5ae57073cb5893ac84ab96030ef9e3
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: REV3NT3CH <rev3nt3ch@gmail.com>
    Signed-off-by: elpaablo <donopwhile1@gmail.com> [adapt to A14 QPR1]

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -420,6 +420,8 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
         }
     };
 
+    private int mEdgeHapticIntensity;
+
     EdgeBackGestureHandler(
             Context context,
             OverviewProxyService overviewProxyService,
@@ -547,7 +549,7 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
         mIsButtonForcedVisible =
                 mGestureNavigationSettingsObserver.areNavigationButtonForcedVisible();
         mIsBackGestureArrowEnabled = mGestureNavigationSettingsObserver.getBackArrowGesture();
-        mIsEdgeHapticEnabled = mGestureNavigationSettingsObserver.getEdgeHapticEnabled();
+        mEdgeHapticIntensity = mGestureNavigationSettingsObserver.getEdgeHapticIntensity();
         if (previousForcedVisible != mIsButtonForcedVisible
                 && mButtonForcedVisibleCallback != null) {
             mButtonForcedVisibleCallback.accept(mIsButtonForcedVisible);
@@ -1125,7 +1127,7 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             if (mAllowGesture) {
                 mEdgeBackPlugin.setIsLeftPanel(mIsOnLeftEdge);
                 mEdgeBackPlugin.setBackArrowVisibility(mIsBackGestureArrowEnabled);
-                mEdgeBackPlugin.setEdgeHapticEnabled(mIsEdgeHapticEnabled);
+                mEdgeBackPlugin.setEdgeHapticIntensity(mEdgeHapticIntensity);
                 mEdgeBackPlugin.onMotionEvent(ev);
                 dispatchToBackAnimation(ev);
             }
